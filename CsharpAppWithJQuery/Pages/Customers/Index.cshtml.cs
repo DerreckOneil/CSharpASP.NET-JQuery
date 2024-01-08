94cbb70bb8d68f3973d61a64d5fecae3937e4cd5
Derreck Oneil <derreckoneil@gmail.com>
2024-01-07 23:32:08 -0500

    Major milestone completed! Reading from the mdb file completed. Next up is to display and give CRUD support

--- CsharpAppWithJQuery/Pages/Customers/Index.cshtml.cs
+++ CsharpAppWithJQuery/Pages/Customers/Index.cshtml.cs
@@ -7,23 +7,49 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CsharpAppWithJQuery.Data;
 using CsharpAppWithJQuery.Models;
+using System.Data.OleDb;
 
 namespace CsharpAppWithJQuery.Pages.Customers
 {
     public class IndexModel : PageModel
     {
         private readonly CsharpAppWithJQuery.Data.CsharpAppWithJQueryContext _context;
+        private readonly string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Personal Projects\\CSharpASP.NET-JQuery\\CsharpAppWithJQuery\\LocalDb\\CustomersDb.mdb";
 
         public IndexModel(CsharpAppWithJQuery.Data.CsharpAppWithJQueryContext context)
         {
             _context = context;
         }
+        public void OnGet()
+        {
 
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT TOP 1 Name FROM Customers";
+                using (OleDbCommand command = new OleDbCommand(query, connection))
+                using (OleDbDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        string columnVal = reader["Name"].ToString();
+                        Console.WriteLine("database first element " + columnVal);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found in the table");
+                    }
+
+                }
+            }
+        }
         public IList<Customer> Customer { get;set; } = default!;
-
+        /*
         public async Task OnGetAsync()
         {
             Customer = await _context.Customer.ToListAsync();
         }
+        */
     }
 }
